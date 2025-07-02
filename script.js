7c5a49b5a836c54bb3380884c07ee5e673d66c34
Miguel <migueljm1008@gmail.com>
2025-07-02 12:48:06 +0200

    add/getData()

--- script.js
+++ script.js
@@ -17,5 +17,33 @@ buttonNext.addEventListener("click", (event) => {
 window.location.href = "../pages/results.html"
 });
 
+/***************** obtencion de datos ***************/
+async function getData() {
+    try{
+        const res= await fetch('https://opentdb.com/api.php?amount=10');
+
+        if (!res.ok) {
+        if (res.status === 404) {
+            throw new Error("Recurso no encontrado (404)");
+        } else if (res.status === 500) {
+            throw new Error("Error en el servidor (500)");
+        } else {
+            throw new Error(`Error HTTP: ${res.status}`);
+        }
+      }
+      const data = await data.json();
+      console.log(data);
+    }catch (error) {
+    if (error.message.includes("404")) {
+      console.error("Error: No se encontró el recurso solicitado.");
+    } else if (error.message.includes("500")) {
+      console.error("Error: Problemas con el servidor.");
+    } else {
+      console.error("Hubo un problema:", error.message);
+    }
+  }
+}
+getData()
+
 
 
