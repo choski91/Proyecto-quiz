c85d44b8bdf89f226280f301569036af4a9e1eba
NazaretMelquiades <nazaretmelquiades@hotmail.com>
2025-07-02 13:44:22 +0200

    prueba espacio

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /************** Botón jugar otra vez *************/
 document.getElementById('playAgain').addEventListener('click', function () {
+
     window.location.href = './question.html';
 });
 
