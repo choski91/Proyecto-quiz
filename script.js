cc1ab7247cab89d5ae89325f43d664bf2c4729b0
NazaretMelquiades <nazaretmelquiades@hotmail.com>
2025-07-02 14:23:00 +0200

    startbutton/change

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
+//Evento botón start
+document.getElementById("start-button").addEventListener("click", (event) => {
+    window.location.href = "../pages/question.html";
+})
+
 /************** Botón jugar otra vez *************/
 document.getElementById('playAgain').addEventListener('click', function () {
 
     window.location.href = './question.html';
 });
 
-//Ufuncionalidad-botones/home
-//Evento botón start
-document.getElementById("start-button").addEventListener("click", (event) => {
-    window.location.href = "../pages/question.html";
-})
 //boton de siguiente en html/question
 
 const buttonNext = document.getElementById("next");
