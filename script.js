c709d62084d5b15b9125d27ffd502d8d5aae921a
Miguel <migueljm1008@gmail.com>
2025-07-03 19:10:59 +0200

    add/manipulacion datos & render preguntas

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+/************** Variables globales **************/
+let pos_pregunta = 0;
+let preguntas = [];
+let resultados = [];
 
-//Ufuncionalidad-botones/home
+//funcionalidad-botones/home
 //Evento botón start
 const startButton = document.getElementById("start-button");
 if (startButton) {
@@ -9,24 +13,35 @@ if (startButton) {
 }
 
 //boton de siguiente en html/question
-
 const buttonNext = document.getElementById("next");
 if (buttonNext) {
   buttonNext.addEventListener("click", (event) => {
   window.location.href = "../pages/results.html";
   })
 }
-// /************** Botón jugar otra vez *************/
+
+ /************** Botón jugar otra vez *************/
 const playAgain = document.getElementById('playAgain');
 if (playAgain){
   playAgain.addEventListener('click', function() {
     window.location.href='./question.html';
   })
 }
+
+/***************** Render primera pregunta ***************/
+const firstquest = document.getElementById('opciones-rta');
+if (firstquest) {
+getData().then(datos => {
+  preguntas = datos;
+  renderPregunta(pos_pregunta, firstquest); // pos 0
+})
+}
+
 /***************** obtencion de datos ***************/
 async function getData() {
     try{
-        const res = await fetch('https://opentdb.com/api.php?amount=10&type=multiple');
+   //     const res = await fetch('https://opentdb.com/api.php?amount=10&type=multiple');
+        const res = await fetch('../datos.json');
 
         if (!res.ok) {
         if (res.status === 404) {
@@ -38,7 +53,8 @@ async function getData() {
         }
       }
       const data = await res.json();
-      return data.results;
+      let datosFinal = manipuDatos(data.results);
+      return datosFinal;
       
     }catch (error) {
     if (error.message.includes("404")) {
@@ -50,17 +66,33 @@ async function getData() {
     }
   }
 }
-getData().then(data => {
-/******************* Manipulacion de datos  ****************/
-  
-  console.log(data);
-  
-  data.map(preguntas => {
-    const pregunta = preguntas.question.toString();
-    const respuesta = [preguntas.correct_answer, preguntas.incorrect_answers[0], preguntas.incorrect_answers[1], preguntas.incorrect_answers[2]];
-    console.log(pregunta, respuesta);
-  })
-})
 
+/******************* Manipulacion de datos  ****************/
+function manipuDatos (dataset) {
+  
+  let mappedData = dataset.map(preguntas => {
+    const pregunta = preguntas.question;
+    const respuesta = [preguntas.incorrect_answers[0], preguntas.incorrect_answers[1], preguntas.incorrect_answers[2]];
+    let num = Math.floor(Math.random() * (respuesta.length + 1));
+    respuesta.splice(num, 0, preguntas.correct_answer)
+    
+    return {pregunta, respuesta, correcta:preguntas.correct_answer};
+  }) 
+  return mappedData;
+} 
+
+/****************** Render Preguntas *******************/
+function renderPregunta(i, container) {
+  console.log(preguntas);
+  container.innerHTML += `
+  <h2>${preguntas[i].pregunta}</h2>
+  <button class='option'>${preguntas[i].respuesta[0]}</button>
+  <button class='option'>${preguntas[i].respuesta[1]}</button>
+  <button class='option'>${preguntas[i].respuesta[2]}</button>
+  <button class='option'>${preguntas[i].respuesta[3]}</button>
+  `
+  
+  //score += 1
+}  
 
 
